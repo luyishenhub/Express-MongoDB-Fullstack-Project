228f9f7d52e1b3861b1e2fbd3bc4944230d0c7ab
Nancy Shen <nancyshen200088@gmail.com>
2021-01-10 20:26:10 -0500

    HTTPS and Secure Communication (pub/private keys, certification)

--- app.js
+++ app.js
@@ -28,6 +28,17 @@ connect.then((db) => {
 
 var app = express();
 
+// Secure traffic only
+app.all('*', (req, res, next) => { //deal with all income traffics
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+    //307: 307 here represents that the target resource resides temporarily under different URL. And the user agent must not change the request method if it reforms in automatic redirection to that URL. So, I'll be expecting user agent to retry with the same method that they have used for the original end point.
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
